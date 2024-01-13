5d13baeaf78bbb2f0680b862aa8b15ab39fc809b
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-13 10:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+const squares = document.getElementsByClassName('square');
+
+const square1 = document.getElementById('square1');
+const square2 = document.getElementById('square2');
+const square3 = document.getElementById('square3');
+
+square1.addEventListener('mouseenter', (e) => handleHover(e));
+square2.addEventListener('mouseenter', (e) => handleHover(e));
+square3.addEventListener('mouseenter', (e) => handleHover(e));
+
+square1.addEventListener('mouseleave', handleLeave);
+square2.addEventListener('mouseleave', handleLeave);
+square3.addEventListener('mouseleave', handleLeave);
+
+function handleHover(event) {
+  for (const square of squares) {
+    if (square === event.target) {
+      // Handle the active square
+    } else {
+      square.classList.add('active');
+    }
+  }
+}
+
+function handleLeave() {
+	for (const square of squares) {
+	    square.classList.remove('active');
+    }
+}
